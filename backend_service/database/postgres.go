71826d914c749318cbe7c09066aed56c67ca353d
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-03-14 14:19:29 +1100

    refactor: use stdlib pgx

--- backend_service/database/postgres.go
+++ backend_service/database/postgres.go
@@ -3,45 +3,39 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func NewPgPool(dsn string) (*pgxpool.Pool, error) {
-	config, err := pgxpool.ParseConfig(dsn)
+func NewPgPool(dsn string) (*sql.DB, error) {
+	// open connection using pgx driver
+	conn, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, err
 	}
 
 	// configure connection pool settings
-	// maximum number of connections in the pool
-	config.MaxConns = 25
-	// minimum number of connections to keep open
-	config.MinConns = 5
-	// maximum lifetime of a connection
-	config.MaxConnLifetime = time.Hour
-	// maximum idle time before closing
-	config.MaxConnIdleTime = 30 * time.Minute
-	// how often to check connection health
-	config.HealthCheckPeriod = 1 * time.Minute
-
-	// create the pool
-	pool, err := pgxpool.NewWithConfig(context.Background(), config)
-	if err != nil {
-		return nil, err
-	}
+	// maximum number of open connections to the database
+	conn.SetMaxOpenConns(25)
+	// maximum number of connections in the idle connection pool
+	conn.SetMaxIdleConns(5)
+	// maximum amount of time a connection may be reused
+	conn.SetConnMaxLifetime(time.Hour)
+	// maximum amount of time a connection may be idle
+	conn.SetConnMaxIdleTime(30 * time.Minute)
 
 	// test the connection
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err = pool.Ping(ctx); err != nil {
-		pool.Close()
+	if err = conn.PingContext(ctx); err != nil {
+		conn.Close()
 		return nil, err
 	}
 
 	log.Println("successfully connected to postgresql")
-	return pool, nil
+	return conn, nil
 }
