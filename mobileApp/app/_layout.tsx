acc4cf7c218d2210a7188b4687f30975d39377c2
klee0126@student.monash.edu <klee0126@student.monash.edu>
2026-03-15 01:41:56 +1100

    feat: connected web socket

--- mobileApp/app/_layout.tsx
+++ mobileApp/app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SensorsProvider } from '../services/sensor-context';
+import { AlertsProvider } from '../services/alert-context';
 
 export default function RootLayout() {
   return (
     <SensorsProvider>
-      <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
+      <AlertsProvider>
+        <StatusBar style="light" />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </AlertsProvider>
     </SensorsProvider>
   );
 }
